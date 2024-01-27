11c2f2be53d7ee7e41a8638ed146908611684674
Alper Demir <105297896+Hololopoto@users.noreply.github.com>
2024-01-27 15:33:29 +0300

    Add Bootstrap and TailwindCSS dependencies

---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="w-full fixed z-40 ">
+      <div className="px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 bg-zinc-900 bg-opacity-90">
+        Zortwrtdfg
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
