b467444456290e5298b5a20e5121050b0783774a
Alper Demir <105297896+Hololopoto@users.noreply.github.com>
2024-01-29 17:11:29 +0300

    Update Banner and Navbar styles

--- src/Banner/Banner.js
+++ src/Banner/Banner.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import axios from "../axios";
 import requests from "../request";
+import { FaPlay } from "react-icons/fa";
+import { IoIosInformationCircleOutline } from "react-icons/io";
+
 function Banner() {
   const [movie, setMovie] = useState([]);
   // const [trailerUrl, setTrailerUrl] = useState("");
@@ -26,10 +29,24 @@ function Banner() {
         alt=""
       />
       <div className="absolute top-[30%] md:top-[40%] ml-4 md:ml-16 content">
-        <h1 className=" text-white text-1xl md:text-5xl lg:text-6xl h-full w-[50%] font-bold drop-shadow-xl ">
-          {movie?.title}
+        <h1 className=" text-white text-1xl md:text-5xl lg:text-6xl h-full w-3/5 font-bold drop-shadow-xl ">
+          {movie?.title?.toUpperCase() ||
+            movie?.name?.toUpperCase() ||
+            movie?.original_name?.toUpperCase()}
         </h1>
-        <p className=" text-white mt-4">{movie?.overview}</p>
+        <p className="  text-white text-[8px] md:text-lg  mt-3 md:mt-8   w-[90%] md:w-[80%] lg:w-[50%]">
+          {movie?.overview}
+        </p>
+        <div className="banner_button flex flex-row items-center mt-3 md:mt-4 gap-3">
+          <button className="text-dark flex flex-row  text-xs lg:text-lg bg-white rounded-md py-1 md:py-2 px-2 md:px-4 w-auto font-semibold items-center hover:bg-slate-300 transition gap-2  ">
+            <FaPlay />
+            Play
+          </button>
+          <button className="text-white flex flex-row text-xs lg:text-lg bg-white bg-opacity-30 rounded-md py-1 md:py-2 px-2 md:px-4 w-auto font-semibold items-center hover:bg-opacity-20 gap-2  ">
+            <IoIosInformationCircleOutline />
+            More Info
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -18,7 +18,7 @@ const Navbar = () => {
 
   return (
     <nav className="w-full fixed z-40 ">
-      <div className="px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 bg-zinc-900 bg-opacity-60">
+      <div className="px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 bg-zinc-900 bg-opacity-40">
         <img
           className="h-4 lg:h-7"
           src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
