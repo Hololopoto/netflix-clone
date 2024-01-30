09c19ea106e4fe8676cfb4b60ee9bf43389cb122
Alper Demir <105297896+Hololopoto@users.noreply.github.com>
2024-01-30 11:04:58 +0300

    Add scroll functionality to movie row

--- src/Row/Row.js
+++ src/Row/Row.js
@@ -1,5 +1,11 @@
 import React, { useEffect, useState } from "react";
 import axios from "../axios";
+import {
+  AiFillStar,
+  AiOutlineArrowRight,
+  AiOutlineArrowLeft,
+} from "react-icons/ai";
+
 import requests from "../request";
 
 function Row({ title, fetchUrl }) {
@@ -13,18 +19,39 @@ function Row({ title, fetchUrl }) {
     };
     fetchData();
   }, [fetchUrl]);
+  const scroolMovieForLeftButton = (e) => {
+    e.target.parentElement.scrollLeft -= 1000;
+  };
+
+  const scroolMovieForRightButton = (e) => {
+    e.target.parentElement.scrollLeft += 1000;
+  };
 
   //   movies.map();
   return (
-    <div className="List-Group">
-      <h2 className="text-white">{title}</h2>
-      <div className="List-Item">
-        <div className="movies_poster">
-          <img
-            src={`https://image.tmdb.org/t/p/w500${movies?.poster_path}`}
-            alt=""
-          />
-        </div>
+    <div className="List-Group overflow-hidden ">
+      <h2 className="text-white text-xl m-4 pl-4">{title}</h2>
+      <div className=" flex flex-row  overflow-hidden  relative scroll-smooth  gap-3 w-full">
+        <button
+          onClick={scroolMovieForLeftButton}
+          className="z-10 w-10 pl-3 bg-black m w-[100px] bg-opacity-60 text-white sticky grid items-center justify-items-center left-0">
+          <AiOutlineArrowLeft className="mr-3" />
+        </button>
+        {movies.map((movie) => (
+          <div className="List-Item  flex flex-row">
+            <div className="movies_poster lg:w-[220px] md:w-[140px]   flex flex-row">
+              <img
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                alt=""
+              />
+            </div>
+          </div>
+        ))}
+        <button
+          onClick={scroolMovieForRightButton}
+          className="z-10 w-10 bg-black pr-3 w-[40px] bg-opacity-60 text-white sticky grid items-center justify-items-center right-0">
+          <AiOutlineArrowRight className="ml-3" />
+        </button>
       </div>
     </div>
   );
